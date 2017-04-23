14fe60d8a21d4f845867a16a88364aaa3c1c6498
Steven Miller <Steven Miller>
2017-04-23 08:53:26 -0400

    cost function passed test cases

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -19,7 +19,7 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
-% h = sigmoid(theta'.*X);
+
 h = sigmoid(X*theta);
 J = (1/m)*(-y'*log(h)-(1-y)'*log(1-h));
 grad = (X'*(h-y))/m
